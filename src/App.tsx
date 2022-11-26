ee239ba12f3f847b92974ffa75e1a75e3ccbf94d
cute3954 <cute3954@naver.com>
2022-11-26 15:52:23 +0900

    Shaderパターン学習

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./styles/style.css";
 import { Cursor, Sizes } from "./types/Common";
 import React, { useEffect, useState } from "react";
 
-import Drei from "./components/drei/Drei";
+import ShaderPatterns from "./components/shaders/ShaderPatterns";
 
 /**
  * App.
@@ -40,7 +40,7 @@ function App() {
     return () => window.removeEventListener("resize", resizeHandler);
   }, []);
 
-  return <Drei />;
+  return <ShaderPatterns />;
 }
 
 export default App;
